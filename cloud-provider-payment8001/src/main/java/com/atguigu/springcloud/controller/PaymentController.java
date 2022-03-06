4b91d32f35adc596378779a157f8d96b28250c31
39636 <Longshine#1>
2022-03-06 11:46:08 +0800

    Eureka集成搭建完成

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,9 +1,9 @@
 package com.atguigu.springcloud.controller;
 
 
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
-import entities.CommonResult;
-import entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
