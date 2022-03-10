20e8164700f488fc83ff9bb64939e23f0eedfde6
39636 <Longshine#1>
2022-03-10 15:09:18 +0800

    eureka 集群搭建注册

---
+++ cloud-eureka-server7002/src/main/java/com/atgigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,22 @@
+package com.atgigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author cj
+ * @Title:
+ * @JdkVersion JDK1.8
+ * @ClassName
+ * @Version 1.0.0
+ * @date 2022/3/1014:59
+ * @Description:
+ */
+@SpringCloudApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
